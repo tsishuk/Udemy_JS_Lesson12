c5d34f60be835078994e36bdced931b71ca7e792
Aleksandr <alextsishuk@gmail.com>
2021-03-04 00:04:51 +0300

    Lesson 23. OOP

--- js/script.js
+++ js/script.js
@@ -1,77 +1,27 @@
 'use strict';
 
-
-// const oldArray = ['a', 'b', 'c'];
-// const newArray = oldArray.slice();
-
-// newArray[1] = 'aascslaknsva';
-
-// console.log(newArray);
-// console.log(oldArray);
-
-
-// const video = ['youtube', 'vimeo', 'rutube'],
-// 			blogs = ['wordpress', 'livejournal', 'blogger'],
-// 			internet = [...video, ...blogs, 'vk', 'fb'];
-
-// // console.log(internet);
-
-// function log(a, b, c) {
-// 	console.log(a);
-// 	console.log(b);
-// 	console.log(c);
-// }
-
-// const num = [2, 5, 7];
-
-// log(...num);
-
-const array = ['a', 'b'];
-
-const newAarray = [...array];
-
-const q = {
-	one: 1,
-	two: 2
+const soldier = {
+	health: 400,
+	armor: 100,
+	sayHello: function(){
+		console.log("Hello from soldier Object");
+	}
 };
 
-const newObj = {...q};
-
-
-// const number = {
-// 	a: 5,
-// 	b:7,
-// 	c: {
-// 		x:10,
-// 		y: 15
-// 	}
+const john = Object.create(soldier);
+// const john = {
+// 	health: 100
 // };
 
-// function copyObj(oldObj){
-// 	const newObj = {};
+// john.__proto__ = soldier;
+// console.log(john);
 
-// 	let key;
-// 	for (key in oldObj){
-// 		newObj[key] = oldObj[key];
-// 	}
-// 	return newObj;
-// }
+// Object.setPrototypeOf(john, soldier);
 
-// const copiedObj = copyObj(number);
+john.sayHello();
 
-// copiedObj.b = 234;
-// copiedObj.c.y = 777;
+// let str = "some";
+// let strObj = new String(str);
 
-// // console.log(number);
-// // console.log(copiedObj);
-
-// const add = {
-// 	d: 2222,
-// 	e: 333
-// };
-
-// const clone = Object.assign({}, add);
-// clone.d = 555;
-
-// console.log(add);
-// console.log(clone);
+// console.log(typeof(str));
+// console.log(typeof(strObj));
