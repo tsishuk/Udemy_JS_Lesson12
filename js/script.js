6b390f52b99c7b80b18463937c22c6d3b471abd0
Aleksandr <alextsishuk@gmail.com>
2021-03-03 11:02:59 +0300

    Lesson18. Practice #3

--- js/script.js
+++ js/script.js
@@ -1,5 +1,16 @@
-const numberOfFilms = +prompt("How much films ?", "");
-console.log(numberOfFilms);
+'use strict';
+
+let numberOfFilms;
+
+function start() {
+  numberOfFilms = +prompt("How much films ?", "");
+
+  while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+    numberOfFilms = +prompt("How much films ?", "");
+  }
+}
+
+start();
 
 const personalMovieDB = {
   count: numberOfFilms,
@@ -9,24 +20,45 @@ const personalMovieDB = {
   privat: false
 };
 
-for (let i = 0; i < 2;){
-  console.log("i before = " + i);
-  const a = prompt("Which film?", ""),
-        b = +prompt("Your grade?", "");
 
-  if ((a==null) || (a.length == 0) || (a.length > 50)) {continue;}
-  personalMovieDB.movies[a] = b;
-  i++;
+function rememberMyFilms() {
+  for (let i = 0; i < 1;) {
+    console.log("i before = " + i);
+    const a = prompt("Which film?", ""),
+      b = +prompt("Your grade?", "");
+
+    if ((a == null) || (a.length == 0) || (a.length > 50)) { continue; }
+    personalMovieDB.movies[a] = b;
+    i++;
+  }
 }
 
+rememberMyFilms();
 
-
-console.log(personalMovieDB); 
-
-if (personalMovieDB.count < 10) {
-  console.log("too few");
-} else if (personalMovieDB.count < 30) {
-  console.log("normal");
-} else {
-  console.log("too much");
+function detectPersonalLevel() {
+  if (personalMovieDB.count < 10) {
+    console.log("too few");
+  } else if (personalMovieDB.count < 30) {
+    console.log("normal");
+  } else {
+    console.log("too much");
+  }
 }
+
+detectPersonalLevel();
+
+function showMyDB(hidden){
+  if (!hidden){12
+    console.log(personalMovieDB);
+  }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+  for (let i = 1; i <= 3; i++){
+    personalMovieDB.genres[i-1] = prompt(`Your favourite genre number ${i}`);
+  }
+}
+
+writeYourGenres();
