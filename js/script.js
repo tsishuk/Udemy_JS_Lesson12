87f5dc6977d48f35503fbeff42c32bbcbc0f2104
Aleksandr <alextsishuk@gmail.com>
2021-03-03 23:17:12 +0300

    Lesson 22. Copying objects

--- js/script.js
+++ js/script.js
@@ -1,42 +1,77 @@
 'use strict';
 
 
+// const oldArray = ['a', 'b', 'c'];
+// const newArray = oldArray.slice();
+
+// newArray[1] = 'aascslaknsva';
+
+// console.log(newArray);
+// console.log(oldArray);
 
 
-const number = {
-	a: 5,
-	b:7,
-	c: {
-		x:10,
-		y: 15
-	}
+// const video = ['youtube', 'vimeo', 'rutube'],
+// 			blogs = ['wordpress', 'livejournal', 'blogger'],
+// 			internet = [...video, ...blogs, 'vk', 'fb'];
+
+// // console.log(internet);
+
+// function log(a, b, c) {
+// 	console.log(a);
+// 	console.log(b);
+// 	console.log(c);
+// }
+
+// const num = [2, 5, 7];
+
+// log(...num);
+
+const array = ['a', 'b'];
+
+const newAarray = [...array];
+
+const q = {
+	one: 1,
+	two: 2
 };
 
-function copyObj(oldObj){
-	const newObj = {};
+const newObj = {...q};
 
-	let key;
-	for (key in oldObj){
-		newObj[key] = oldObj[key];
-	}
-	return newObj;
-}
 
-const copiedObj = copyObj(number);
+// const number = {
+// 	a: 5,
+// 	b:7,
+// 	c: {
+// 		x:10,
+// 		y: 15
+// 	}
+// };
 
-copiedObj.b = 234;
-copiedObj.c.y = 777;
+// function copyObj(oldObj){
+// 	const newObj = {};
 
-// console.log(number);
-// console.log(copiedObj);
+// 	let key;
+// 	for (key in oldObj){
+// 		newObj[key] = oldObj[key];
+// 	}
+// 	return newObj;
+// }
 
-const add = {
-	d: 2222,
-	e: 333
-};
+// const copiedObj = copyObj(number);
 
-const clone = Object.assign({}, add);
-clone.d = 555;
+// copiedObj.b = 234;
+// copiedObj.c.y = 777;
 
-console.log(add);
-console.log(clone);
+// // console.log(number);
+// // console.log(copiedObj);
+
+// const add = {
+// 	d: 2222,
+// 	e: 333
+// };
+
+// const clone = Object.assign({}, add);
+// clone.d = 555;
+
+// console.log(add);
+// console.log(clone);
