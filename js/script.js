8a9b808886083c40075cfa0031ca383c5dd14a22
Aleksandr <alextsishuk@gmail.com>
2021-03-03 14:17:53 +0300

    Lesson 20. Objects

--- js/script.js
+++ js/script.js
@@ -1,25 +1,43 @@
 'use strict';
 
-function first() {
-  setTimeout(function() {
-    console.log(1);
-  }, 500);
-}
 
-function second() {
-  console.log(2);
-}
 
-first();
-second();
+const parametr = {
+	aname: "Tool",
+	aweight: 12345,
+	aobject: {
+		sfirst: "abcd",
+		ssecond: "gert",
+		sthird: 777
+	},
+	alast: "theend",
+	makeTest: function(){
+		console.log("Test");
+	}
+};
 
-function learnJS(lang, callback){
-  console.log(`Я учу ${lang}`);
-  callback();
-}
 
-function done() {
-  console.log('Done');
-}
+parametr.makeTest();
 
-learnJS("JavaScript", done);
+const {sfirst, ssecond, sthird} = parametr.aobject;
+
+console.log(sfirst);
+// console.log(Object.keys(parametr).length);
+
+
+
+
+
+//	--------------------------------------------------------------
+// for (let key in parametr){
+// 	if (typeof(parametr[key]) == 'object'){
+// 		console.log(`key = ${key}, - insight object: -`);
+// 		for (let inkey in parametr[key]){
+// 			console.log(`\tinkey = ${inkey}, value = ${parametr[key][inkey]}`);
+// 		}
+// 	}
+// 	else {
+// 		console.log(`key = ${key}, value = ${parametr[key]}`);
+// 	}
+// }
+//	--------------------------------------------------------------
