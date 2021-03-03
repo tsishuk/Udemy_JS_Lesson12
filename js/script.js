45528d8401ff9d61ea3a8bc42b4a7f6a471b1f55
Aleksandr <alextsishuk@gmail.com>
2021-03-03 08:46:55 +0300

    Before 18 lesson. Add cycles and check

--- js/script.js
+++ js/script.js
@@ -9,12 +9,24 @@ const personalMovieDB = {
   privat: false
 };
 
-const a = prompt("Which film?", ""),
-      b = prompt("How much?", ""),
-      c = prompt("Which film?", ""),
-      d = prompt("How much?", "");
+for (let i = 0; i < 2;){
+  console.log("i before = " + i);
+  const a = prompt("Which film?", ""),
+        b = +prompt("Your grade?", "");
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+  if ((a==null) || (a.length == 0) || (a.length > 50)) {continue;}
+  personalMovieDB.movies[a] = b;
+  i++;
+}
 
-console.log(personalMovieDB);
+
+
+console.log(personalMovieDB); 
+
+if (personalMovieDB.count < 10) {
+  console.log("too few");
+} else if (personalMovieDB.count < 30) {
+  console.log("normal");
+} else {
+  console.log("too much");
+}
