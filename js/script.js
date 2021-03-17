e700642f924d0fc42a3f7779c2ef3f0b7a362503
Aleksandr <alextsishuk@gmail.com>
2021-03-17 23:39:42 +0300

    Experiments with data types

--- js/script.js
+++ js/script.js
@@ -13,7 +13,8 @@ const wrapper = document.querySelector('.wrapper');
 
 box.style.cssText = 'background-color: green; width: 200px';
 
-btns[1].style.borderRadius = '100%';
+console.log(btns);
+btns[1].style.width = '100px';
 circles[1].style.backgroundColor = 'red';
 
 // for (let i = 0; i < hearts.length; i++) {
@@ -44,6 +45,110 @@ wrapper.append(div);
 
 // wrapper.appendChild(div);
 
-div.innerHTML = "<h1>Hello</h1>" ;
+div.innerHTML = "<h1>Hello there</h1>";
 // div.textContent = "Hello";
-div.insertAdjacentHTML('afterend', '<h2>Halo</h2>');
+div.insertAdjacentHTML('afterend', '<h2>Halo from there</h2>');
+
+
+function sleepMy(milliseconds) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < milliseconds);
+}
+
+sleepMy(3000);
+btns[1].click();
+
+
+
+
+
+
+
+
+
+
+// let obj = {
+//   names: "Aary",
+//   age: 45,
+//   "big apple" : "Juicyfruit"
+// };
+
+// obj.time = 567;
+
+// for (let k in obj){
+//   console.log(k + ": " + obj[k]);
+//   // console.log(obj[k]);
+// }
+
+
+
+
+
+// let user = {
+//   name: "Ivan"
+// };
+
+// let perm1 = {
+//   can: "yes",
+// };
+
+// let perm2 = {
+//   just: "fun"
+// };
+// console.log(user);
+
+// Object.assign(user, perm1, perm2);
+
+// console.log(user);
+
+
+//
+//------------ Addition -----------
+//
+// let set = new Set();
+
+// let john = { name: "John" };
+// let pete = { name: "Pete" };
+// let mary = { name: "Mary" };
+
+// // считаем гостей, некоторые приходят несколько раз
+// set.add(john);
+// set.add(pete);
+// set.add(mary);
+// set.add(john);
+// set.add(mary);
+
+// // set хранит только 3 уникальных значения
+//  console.log(set.size); // 3
+
+// for (let user of set) {
+//    console.log(user.name); // John (потом Pete и Mary)
+// }
+
+
+
+
+
+// let set = new Set(["апельсин", "яблоко", "банан"]);
+
+// for (let value of set) {console.log(value);}
+
+// // то же самое с forEach:
+// set.forEach((value, valueAgain, set) => {
+//   console.log(value);
+// });
+
+
+
+
+
+// // 0 соответствует 01.01.1970 UTC+0
+// let Jan01_1970 = new Date(0);
+// console.log( Jan01_1970 );
+
+// // теперь добавим 24 часа и получим 02.01.1970 UTC+0
+// let Jan02_1970 = new Date(24 * 3600 * 1000);
+// console.log( Jan02_1970 );
